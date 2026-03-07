1ec9791c9fe2fdce5c6dc0cff092a04b7ac1b392
Alexey Malov <malov.aleksey@gmail.com>
2026-03-07 13:49:05 +0300

    Добавил документ со стилем кодирования

---
+++ coding_style/coding_style.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Constants should be in upper case with words separated by underscores
+const int MAX_SIZE = 100;
+
+struct StructNameInUpperCamelCase
+{
+    int memberNameInLowerCamelCase;
+};
+
+void FunctionNameUnUpperCamelCase(int varNameInLowerCamelCase)
+{
+}
+
+int main(void)
+{
+    return 0;
+}
