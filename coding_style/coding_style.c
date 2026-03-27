ea44c8d1c54f392ddb0b2544fbf086c4f5fda34c
Alexey Malov <malov.aleksey@gmail.com>
2026-03-27 23:14:36 +0300

    Обновил coding style

--- coding_style/coding_style.c
+++ coding_style/coding_style.c
@@ -1,28 +1,44 @@
-#include <stdio.h>
+#include <stdio.h> // Leave at least one empty line after the include block.
 
 // Constants should be in upper case with words separated by underscores
 const int MAX_SIZE = 100;
 
+// Struct name should be in UpperCamelCase
 struct StructNameInUpperCamelCase
 {
-    int memberNameInLowerCamelCase;
+	int memberNameInLowerCamelCase;
 };
 
+// Function name should be in UpperCamelCase
 void FunctionNameUnUpperCamelCase(int varNameInLowerCamelCase)
 {
+	const int localMaxSize = 10; // Local const name should be in lowerCamelCase
+	if (varNameInLowerCamelCase > localMaxSize) // The opening bracket is at the next line
+	{
+		printf("%d is grater than %d\n", varNameInLowerCamelCase, localMaxSize);
+	}
+	else
+	{
+		printf("%d is less or equal to %d\n", varNameInLowerCamelCase, localMaxSize);
+	}
+}
+
+void PrintArray(const int* arr, int size) // The pointer symbol "*" is always close to the data type, not to the variable
+{
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
 }
 
 int main(void)
 {
-    int ch = getchar();
-    if (ch == EOF)
-    {
-        printf("End of file reached\n");
-        return 1;
-    }
-    else
-    {
-        printf("Character read: %c\n", ch);
-    }
-    return 0;
+	int numbers[] = { 10, 20, 30, 40, 50 };
+	int* ptr = numbers;
+
+	PrintArray(ptr, 5);
+
+	FunctionNameUnUpperCamelCase(5);
+	return 0;
 }
